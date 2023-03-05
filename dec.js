df532bdb318b0e03a1e23d25ba5bcb9acfb29cc5
Pablo Gonzalez <ezekielgonzalezr@gmail.com>
2023-03-04 18:55:22 -0600

    added variables

--- dec.js
+++ dec.js
@@ -4,14 +4,18 @@ const december = new Date("December 1 2023 00:00:00");
 let countdown =() =>{
     const current = new Date();
     const left = december - current;
+    let day = document.querySelector("#day");
+    let hour = document.querySelector("#hour");
+    let min = document.querySelector("#min");
+    let sec = document.querySelector("#sec");
     const d = Math.floor(left / 1000 / 60 / 60 / 24);   
     const h = Math.floor((left / 1000 / 60 / 60) % 24); 
     const m = Math.floor((left / 1000 / 60) % 60);
     const s = Math.floor((left / 1000 ) % 60); 
-    document.getElementById("day").innerText = d;
-    document.getElementById('hour').innerText = h < 10 ? "0" + h : h;
-    document.getElementById('min').innerText = m < 10 ? "0" + m : m;
-    document.getElementById('sec').innerText = s < 10 ? "0" + s : s;
+    day.innerText = d;
+    hour.innerText = h < 10 ? "0" + h : h;
+    min.innerText = m < 10 ? "0" + m : m;
+    sec.innerText = s < 10 ? "0" + s : s;
 }
 setInterval(countdown, 1000);
 
--- xmas.js
+++ xmas.js
@@ -3,14 +3,18 @@ const xmas = new Date("December 24 2023 00:00:00");
 let xmasCountdown = () =>{
     const sitting = new Date();
     const remaining = xmas - sitting;
+    let day = document.querySelector("#xday");
+    let hour = document.querySelector("#xhour");
+    let min = document.querySelector("#xmin");
+    let sec = document.querySelector("#xsec");
     const d = Math.floor(remaining / 1000 / 60 / 60 / 24);   
     const h = Math.floor((remaining / 1000 / 60 / 60) % 24); 
     const m = Math.floor((remaining / 1000 / 60) % 60);
     const s = Math.floor((remaining / 1000 ) % 60); 
-    document.getElementById('xday').innerHTML = d;
-    document.getElementById('xhour').innerHTML = h < 10 ? "0" + h : h;
-    document.getElementById('xmin').innerHTML = m < 10 ? "0" + m : m;
-    document.getElementById('xsec').innerHTML = s < 10 ? "0" + s : s;
+    day.innerHTML = d;
+    hour.innerHTML = h < 10 ? "0" + h : h;
+    min.innerHTML = m < 10 ? "0" + m : m;
+    sec.innerHTML = s < 10 ? "0" + s : s;
 }
 setInterval(xmasCountdown, 1000);
 let  playSound2 =() =>{
